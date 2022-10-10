0cc3cba768f0a8f18b62fd2d0046d44f48e39ea2
rravelli <remi.ravelli@gmail.com>
2022-10-10 10:43:29 +0200

    Ajout couleur noeud. Amélioration esthétiques

--- src/main/java/ObjetsGraph/Noeud.java
+++ src/main/java/ObjetsGraph/Noeud.java
@@ -4,6 +4,11 @@
  */
 package ObjetsGraph;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
 
 /**
@@ -14,9 +19,11 @@ public class Noeud extends Ellipse2D.Double {
     
     private double cx;
     private double cy;
+    public Color color;
     
     public Noeud(){
         super();
+        this.
         cx = 0;
         cy = 0;
     }
@@ -25,6 +32,14 @@ public class Noeud extends Ellipse2D.Double {
         super(cx-width/2,cy-height/2,width,height);
         this.cx = cx;
         this.cy = cy;
+        this.color = Color.WHITE;
+    }
+    
+    public Noeud(double cx, double cy, double width, double height, Color color){
+        super(cx-width/2,cy-height/2,width,height);
+        this.cx = cx;
+        this.cy = cy;
+        this.color = color;
     }
     
     public void updateSize(double width, double height){
@@ -39,5 +54,34 @@ public class Noeud extends Ellipse2D.Double {
         cy = y;
         super.x = cx-width/2;
         super.y = cy-height/2;
+    }
+    
+    public void paint(Graphics2D g, boolean multiSelected, String label) {
+        g.setStroke(new BasicStroke(7));
+        //Outline
+        if(multiSelected){
+            g.setPaint(Color.GREEN); 
+        }else{
+            g.setPaint(Color.BLACK);
+        }
+        g.draw(this);
+        g.setStroke(new BasicStroke(1));
+        //Inside
+        g.setPaint(color);
+        
+        g.fill(this);
+        //Label
+        if (label != null) {
+            Font font = new Font("Arial",Font.PLAIN,15);
+            FontMetrics metrics = g.getFontMetrics(font);
+            // Determine the X coordinate for the text
+            int x = (int) (this.x + (this.width - metrics.stringWidth(label)) / 2);
+            // Determine the Y coordinate for the text (note we add the ascent, as in java 2d 0 is top of the screen)
+            int y = (int) (this.y + ((this.height - metrics.getHeight()) / 2) + metrics.getAscent());
+            // Set the font
+            g.setColor(Color.BLACK);
+            g.setFont(font);
+            g.drawString(label,x,y);
+        }
     }
 }
