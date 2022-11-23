c55e5370815db184be5f8e9f0d0c7071f422d365
Inky19 <f.marie260@gmail.com>
2022-11-23 01:47:19 +0100

    Added missing files

---
+++ src/main/java/Inforeg/Algo/AlgorithmS.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Inforeg.Algo;
+
+import Inforeg.Draw.Draw;
+import Inforeg.ObjetGraph.Node;
+
+/**
+ *
+ * @author inky19
+ */
+public interface AlgorithmS {
+    
+    public boolean process(Draw d, Node src);
+}
---
+++ src/main/java/Inforeg/Algo/DFS.java
@@ -0,0 +1,19 @@
+package Inforeg.Algo;
+
+import Inforeg.Draw.Draw;
+
+/**
+ *
+ * @author inky19
+ */
+public class DFS extends Algorithm {
+
+    @Override
+    public boolean process(Draw d) {
+        
+        return false;
+        
+    }
+
+    
+}
