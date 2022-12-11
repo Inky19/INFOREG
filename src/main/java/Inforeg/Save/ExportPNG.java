1f29fe6467cd0ff87260de6e5802589b7efd6ce0
Inky19 <f.marie260@gmail.com>
2022-12-12 00:16:10 +0100

    Added PNG export (files)

---
+++ src/main/java/Inforeg/Save/ExportPNG.java
@@ -0,0 +1,38 @@
+package Inforeg.Save;
+
+import Inforeg.Draw.Draw;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+
+/**
+ *
+ * @author inky19
+ */
+public abstract class ExportPNG {
+    
+    public static void export(Draw d){
+        JFileChooser fileExplorer = new JFileChooser();
+        int res = fileExplorer.showOpenDialog(null);
+        if (res == JFileChooser.APPROVE_OPTION) {
+            d.prepareExport();
+            File file = fileExplorer.getSelectedFile();
+            String[] filePath = Utils.formatPath(file, "png");
+            File formatFile = new File(filePath[1]);
+            BufferedImage im = new BufferedImage(d.getWidth(), d.getHeight(), BufferedImage.TYPE_INT_ARGB);
+            Graphics2D gfx = im.createGraphics();
+            d.paint(gfx);
+            try {
+                ImageIO.write(im, "PNG", formatFile);
+            } catch (IOException ex) {
+                Logger.getLogger(ExportPNG.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            d.restoreAfterExport();
+        }
+    }
+}
